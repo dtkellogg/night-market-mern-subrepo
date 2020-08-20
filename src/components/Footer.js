d3bddd9087d4e376f435e8efab1d66a845f573da
Toshi <dtkellogg10@gmail.com>
2020-08-20 13:08:05 -0700

    Initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
-      <span>&copy; 2020 by Damian Toshiya Kellogg. All rights reserved...</span>
+      <span>&copy; 2020 by Damian Toshiya Kellogg. All rights reserved.</span>
     </div>
   );
 }
